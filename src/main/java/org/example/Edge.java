63a523d178f797630623c56dfe545ea4c69d8b9a
zuxinchen <chenz@farmingdale.edu>
2024-10-22 10:10:09 -0400

    ok

---
+++ src/main/java/org/example/Edge.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Create the Edge class with the following members:
+ * • Member variables for source, destination, and weight. They should all have the data type int.
+ * These variables should have package scope (leave out the access modifier for package scope).
+ * • Constructor that sets all three member variables.
+ */
+public class Edge {
+    private int source;
+    private int destination;
+    private int weight;
+}
